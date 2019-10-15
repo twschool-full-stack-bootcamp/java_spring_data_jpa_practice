ae08517a6c2212aeabfd5b32438c4b862439e5ff
Songbai Yan <colorguitar@hotmail.com>
2019-10-15 16:08:10 +0800

    answer

--- src/main/java/com/tw/apistackbase/core/Company.java
+++ src/main/java/com/tw/apistackbase/core/Company.java
@@ -1,6 +1,11 @@
 package com.tw.apistackbase.core;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Company {
+    @Id
     private Long id;
 
     private String name;
